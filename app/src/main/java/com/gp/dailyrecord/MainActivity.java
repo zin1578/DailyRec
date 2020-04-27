ed877f8fa96d14145c59a0592ea2b2ee896db721
hyebeen7503 <gpqls545@naver.com>
2020-04-27 13:54:20 +0900

    Update MainActivity.java

--- app/src/main/java/com/gp/dailyrecord/MainActivity.java
+++ app/src/main/java/com/gp/dailyrecord/MainActivity.java
@@ -159,7 +159,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
-    /* ##캘린더변경마스터로보내기
+    /* ##캘린더변경마스터로보내기_내용추가
    public void onButtonDiaryClicked(View v){
         Intent intent = new Intent(getApplicationContext(), CalenderActivity.class);
         startActivityForResult(intent, REQUEST_CODE_MENU);
