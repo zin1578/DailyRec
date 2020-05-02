48644201c0dca42bc2327f7d056d1f37d8061460
zin1578 <zin1578@github.com>
2020-05-02 22:24:36 +0900

    카카오 맵 Fragment1에 추가

--- app/src/main/java/com/gp/dailyrecord/FirstFragment.java
+++ app/src/main/java/com/gp/dailyrecord/FirstFragment.java
@@ -9,11 +9,16 @@ import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
 
 public class FirstFragment extends Fragment {
     // Store instance variables
     private String title;
     private int page;
+
 
     // newInstance constructor for creating fragment with arguments
     public static FirstFragment newInstance(int page, String title) {
@@ -39,9 +44,28 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_first, container, false);
-        EditText tvLabel = (EditText) view.findViewById(R.id.editText);
-        tvLabel.setText(page + " -- " + title);
-        return view;
+        //EditText tvLabel = (EditText) view.findViewById(R.id.editText);
+        // tvLabel.setText(page + " -- " + title);
 
+        MapView mapView = new MapView(this.getActivity());
+
+        ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+        // 중심점 변경 - 예제 좌표는 서울 남산
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304), true);
+        // 줌 레벨 변경
+        mapView.setZoomLevel(4, true);
+
+        //마커 찍기
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
+        return view;
     }
 }
